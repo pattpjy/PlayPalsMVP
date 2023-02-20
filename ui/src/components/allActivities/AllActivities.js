115c758b47638757a7029c984775268ef14a58c2
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-20 09:52:54 -0700

    Create activity card

--- ui/src/components/allActivities/AllActivities.js
+++ ui/src/components/allActivities/AllActivities.js
@@ -3,11 +3,29 @@ import "./AllActivities.scss";
 import Filter from "../filter/Filter";
 import ActivityCard from "../thumbnailActivity/ActivityCard";
 
-export default function AllActivities() {
-    return (
-        <section className="all-activities-section">
-            <Filter />
-            <ActivityCard />
-        </section>
-    );
+
+export default function AllActivities({ allActivities}) {
+	const activityCards = allActivities.map(activity => {
+		return (
+			<ActivityCard
+				key={activity.id}
+				id={activity.id}
+				startAge={activity.startAge}
+				endAge={activity.endAge}
+				name={activity.name}
+				matertials={activity.materials}
+				instructions={activity.instructions}
+				indoor={activity.indoor}
+				outdoor={activity.outdoor}
+				motorSkill={activity.motorSkill} />
+		)
+	})
+	return (
+		<section className="all-activities-section">
+			<Filter />
+			<div className='activity-container'>
+				{activityCards}
+			</div>
+		</section>
+	);
 }
--- ui/src/components/app/App.js
+++ ui/src/components/app/App.js
@@ -7,14 +7,21 @@ import AllActivities from "../allActivities/AllActivities";
 import ToDos from "../todo/ToDos";
 import ActivityInfoView from "../activityInfoView/ActivityInfoView";
 import { Form } from 'react-router-dom';
+import activities from "../../api/dummyData";
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [allActivities, setAllActivities] = useState([])
+
+	useEffect(() => {
+		setAllActivities(activities)
+	})
   return (
     <main>
       <HeaderNav />
 			<Routes>
 				<Route path='/' element={<LandingPage />}/>
-				<Route path='/AllActivities' element={<AllActivities />}/>
+				<Route path='/AllActivities' element={<AllActivities allActivities={allActivities}/>}/>
 				<Route path='/ToDos' element={<ToDos />} />
 				<Route path='/ActivityInfoView' element={<ActivityInfoView />}/>
 				{/* <Route path='*' element={<ErrorPage />}/> */}
