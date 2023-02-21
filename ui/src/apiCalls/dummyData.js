dc9f2e5814c6e69396078673d0e64e1c81593dfc
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-20 17:55:03 -0700

    Create one single view

--- ui/src/apiCalls/dummyData.js
+++ ui/src/apiCalls/dummyData.js
@@ -8,7 +8,7 @@ const activitiesData = {
       name: "paper cutting",
       materials: ["paper", "child-scissor", "bin"],
       instructions:
-        "1: have child hold scissors with whichever hand they choose, 2: cut paper into whatever shape they want in any size they want",
+        ['Have child hold scissors with whichever hand they choose', 'cut paper into whatever shape they want in any size they want'],
       indoor: true,
       outdoor: false,
       motorSkills: ["fine"],
@@ -20,7 +20,7 @@ const activitiesData = {
       name: "Sensory bags and bins",
       materials: ["resealable bag", "bin", "dry pasta"],
       instructions:
-        "Put different textured objects in a bag and let your child pull them out one by one. Just make sure that objects are nontoxic and big enough not to be a choking hazard, since they are likely to end up in your child's mouth",
+        ["Put different textured objects in a bag and let your child pull them out one by one", "Make sure that objects are nontoxic and big enough not to be a choking hazard, since they are likely to end up in your child's mouth"],
       indoor: true,
       outdoor: false,
       motorSkill: ["fine"],
@@ -37,7 +37,7 @@ const activitiesData = {
         "outdoor materials: towels, blow up toys",
       ],
       instructions:
-        "Toss blankets over furniture to create tunnels tto crawl through, toss pillows on the floor to jump onto if child is older or to crawl over if younger, a prize is placed at the end of the course or make the course a circle, go outside for more space",
+        ["Toss blankets over furniture to create tunnels to crawl through","toss pillows on the floor to jump onto if child is older or to crawl over if younger","Place a prize at the end of the course or make the course a circle", "go outside for more space"],
       indoor: true,
       outdoor: true,
       motorSkill: ["gross"],
@@ -49,7 +49,7 @@ const activitiesData = {
       name: "pompom whisk",
       materials: ["whisk", "pompom"],
       instructions:
-        "Place pompoms into whisk and have child try and pull them out, continue to add pompoms until the child is no longer interested",
+        ["Place pompoms into whisk and have child try and pull them out", "Continue to add pompoms until the child is no longer interested"],
       indoor: true,
       outdoor: false,
       motorSkill: ["fine"],
@@ -61,7 +61,7 @@ const activitiesData = {
       name: "running sponge",
       materials: ["bucket", "water", "sponge"],
       instructions:
-        "Fill up a bucket with water and give child a big sponge, dip it in the bucket and then run to another bucket and squeeze the water into it. Repeat until all the water has transferred between buckets",
+        ["Fill up a bucket with water and give child a big sponge", "Dip it in the bucket and then run to another bucket and squeeze the water into it", "Repeat until all the water has transferred between buckets"],
       indoor: false,
       outdoor: true,
       motorSkill: ["fine", "gross"],
@@ -77,7 +77,7 @@ const activitiesData = {
         "cardboard box or butcher paper",
       ],
       instructions:
-        "Dip cars in paint and place cars on paper, push cars along or send cars racing down the paper to create fun and creative art peices",
+        ["Dip cars in paint and place cars on paper", "Push cars along or send cars racing down the paper to create fun and creative art peices"],
       indoor: false,
       outdoor: true,
       motorSkill: ["fine"],
@@ -93,7 +93,7 @@ const activitiesData = {
         "paint and brushes",
       ],
       instructions:
-        "Place ice in bin and place on well worn towel to catch extra water and paint, start by painting the ice, as the ice melts the child can watch everything turn different colors",
+        ["Place ice in bin and place on well worn towel to catch extra water and paint", "Start by painting the ice, as the ice melts the child can watch everything turn different colors"],
       indoor: true,
       outdoor: true,
       motorSkill: ["fine"],
@@ -111,7 +111,7 @@ const activitiesData = {
         "cutting board",
       ],
       instructions:
-        "Have your child pick crackers and berries, cut them to size and arrange on the plate. To finish work cycle, have them put away dirty dishes.",
+        ["Have your child pick crackers and berries", "Cut them to size and arrange on the plate", "To finish work cycle, have them put away dirty dishes."],
       indoor: true,
       outdoor: false,
       motorSkill: ["fine"],
@@ -123,7 +123,7 @@ const activitiesData = {
       name: "ice cube transfer ",
       materials: ["color ice cube", "water", "2 large bowls", "slotted spoon"],
       instructions:
-        "Fill ice cube trays with water and food coloring. Fill one with water and dump the ice cubes in. Leave the other bowl empty. Challenge your child to transfer all of the ice cubes from one bowl to the other with slotted spoon",
+        ["Fill ice cube trays with water and food coloring", "Fill one with water and dump the ice cubes in", "Leave the other bowl empty", "Challenge your child to transfer all of the ice cubes from one bowl to the other with slotted spoon"],
       indoor: true,
       outdoor: true,
       motorSkill: ["gross"],
@@ -135,7 +135,7 @@ const activitiesData = {
       name: "Chasing bubbles",
       materials: ["lots of bubble solutions, outdoor space"],
       instructions:
-        "Warning!, there will be a lot of spillage, motivate your child to move, and run into a certain direction, as well as jump and catch bubbles",
+        ["Warning!, there will be a lot of spillage, motivate your child to move, and run into a certain direction, as well as jump and catch bubbles"],
       indoor: false,
       outdoor: true,
       motorSkill: ["gross"],
--- ui/src/components/activityInfoView/ActivityInfoView.js
+++ ui/src/components/activityInfoView/ActivityInfoView.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ActivityInfoView.scss";
+import activitiesData from "../../apiCalls/dummyData.js";
 import { AiOutlineCheckCircle } from "react-icons/ai"
 import { AiFillCheckCircle } from "react-icons/ai"
 import { AiOutlineHeart } from "react-icons/ai"
@@ -7,17 +8,26 @@ import { AiFillHeart } from "react-icons/ai"
 import { BsBookmark } from "react-icons/bs"
 import { BsFillBookmarkFill } from "react-icons/bs"
 
-export default function ActivityInfoView({ name, startAge, endAge, materials, instructions, image}) {
+export default function ActivityInfoView({ name, startAge, endAge, materials, instructions, image }) {
+	const [singleActivity, setSingleActivity] = useState(activitiesData.activities[0])
 	return (
-		<section>
-			<p>{name}</p>
-			<p>{startAge}-{endAge}</p>
-			<img src={image} alt={name} />
-			<p>{materials}</p>
-			<p>{instructions}</p>
-			<BsBookmark type='button' alt='blankBookmark' />
-			<AiOutlineCheckCircle type='button' alt='blankCheckmark' />
-			<AiOutlineHeart type='button' alt='blankHeart' />
+		<section className='activity-info-view-container'>
+			<div className='heart-and-name'>
+				<AiOutlineHeart type='button' alt='blankHeart' />
+				<h3 className='name'>{singleActivity.name}</h3>
+			</div>
+			<div className='picture-buttons'>
+				<img src={singleActivity.image} alt={singleActivity.name} />
+				<div className='buttons'>
+					<AiOutlineCheckCircle type='button' alt='blankCheckmark' />
+					<BsBookmark type='button' alt='blankBookmark' />
+				</div>
+			</div>
+			<div className='information'>
+				<p>{singleActivity.startAge} months -{singleActivity.endAge} months</p>
+				<p>MATERIALS <br /> {singleActivity.materials}</p>
+				<p>INSTRUCTIONS <br /> {singleActivity.instructions}</p>
+			</div>
 		</section>
 	)
 }
