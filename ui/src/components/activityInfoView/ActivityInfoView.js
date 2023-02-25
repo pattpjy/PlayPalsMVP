348722122021a0f9cf1ca2a24a0367fb11f17f20
Sarah Hampton <stoluba@gmail.com>
2023-02-25 18:01:27 -0500

    Clean up

--- ui/src/components/activityInfoView/ActivityInfoView.js
+++ ui/src/components/activityInfoView/ActivityInfoView.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import "./ActivityInfoView.scss";
-import { AiFillAccountBook, AiFillCheckCircle, AiOutlineHeart, AiFillHeart, AiOutlineCheckCircle } from "react-icons/ai"
+import { AiFillCheckCircle, AiOutlineHeart, AiFillHeart, AiOutlineCheckCircle } from "react-icons/ai"
 import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs"
+import { useLocation } from "react-router-dom";
 
-export default function ActivityInfoView({ singleActivity }) {
+export default function ActivityInfoView() {
+	const location = useLocation()
+	const currentId = location.state.id
+	const allActivities = location.state.activities
+	console.log('location', allActivities)
+	console.log('currentid', currentId)
+	const currentActivity = allActivities.find(activity => activity.id === currentId)
+	console.log('currentActivity', currentActivity)
 	const [toggle, setToggle] = useState(false)
 
 	const handleToggle = () => {
 		setToggle(!toggle)
 	}
 
+
 	return (
 		<section className='activity-info-view-container'>
 			<div className='heart-and-name'>
 				{!toggle && <AiOutlineHeart onClick={handleToggle} type='button' alt='blankHeart' />}
 				{toggle && <AiFillHeart onClick={handleToggle} type='button' alt='filledHeart' />}
-				<h3 className='name'>{singleActivity.name}</h3>
+				<h3 className='name'>{currentActivity.name}</h3>
 			</div>
 			<div className='picture-buttons'>
-				<img src={singleActivity.image} alt={singleActivity.name} />
+				<img src={currentActivity.img_url} alt={currentActivity.name} />
 				<div className='buttons'>
 					{!toggle && <AiOutlineCheckCircle onClick={handleToggle} type='button' alt='blankCheckmark' />}
 					{toggle && <AiFillCheckCircle onClick={handleToggle} type='button' alt='blankCheckmark' />}
@@ -27,9 +36,9 @@ export default function ActivityInfoView({ singleActivity }) {
 				</div>
 			</div>
 			<div className='information'>
-				<p>{singleActivity.startAge} months -{singleActivity.endAge} months</p>
-				<p>MATERIALS <br /> {singleActivity.materials}</p>
-				<p>INSTRUCTIONS <br /> {singleActivity.instructions}</p>
+				<p>{currentActivity.start_age} months -{currentActivity.end_age} months</p>
+				<p>MATERIALS <br /> {currentActivity.materials}</p>
+				<p>INSTRUCTIONS <br /> {currentActivity.instructions}</p>
 			</div>
 		</section>
 	)
